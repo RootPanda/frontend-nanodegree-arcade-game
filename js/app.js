58e58813685bceece428c7d20bfeddc2fca8c7da
Alex Leung <Alex Leung>
2016-09-03 17:14:39 -0400

    Instantiate entities (player and enemies) and implement basic methods.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+var Enemy = function(x, y, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+
+    this.x = x;
+    this.y = y;
+
+    this.speed = speed;     // Speed
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+
+    // TODO: player collision
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +27,58 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// This is us.
+var Player = function(x, y) {
+    this.sprite = 'images/char-horn-girl.png';
 
+    this.x = x;
+    this.y = y;
+
+    this.deltaX = 70
+    this.deltaY = 70
+};
+
+// Handle collisions here.
+Player.prototype.update = function() {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    switch(key) {
+    case 'up': 
+        console.log("PLayer moved up!");
+        this.y -= this.deltaY;
+        break;
+    case 'left': 
+        this.x -= this.deltaX;
+        console.log("PLayer moved left!");
+        break;
+    case 'down': 
+        this.y += this.deltaY;
+        console.log("PLayer moved down!");
+        break;
+    case 'right': 
+        this.x += this.deltaX;
+        console.log("PLayer moved right!");
+        break;
+    default:
+        break;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player(100, 100);
+
+var allEnemies = [];
+allEnemies.push(new Enemy(0, 60, 20));
+allEnemies.push(new Enemy(0, 140, 20));
+allEnemies.push(new Enemy(0, 220, 20));
 
 
 // This listens for key presses and sends the keys to your
